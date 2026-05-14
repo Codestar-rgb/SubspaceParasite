c2dbbec029617cc885d4130c6cef54a52b76ef5f
Z User <z@container>
2026-05-14 13:19:15 +0000

    064540c1-3f2f-4633-b669-e2062125a60f

---
+++ db/KirinGeoModel.java
@@ -0,0 +1,29 @@
+package com.example.srparasites.client.model;
+
+import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib.model.GeoModel;
+import com.example.srparasites.entity.KirinEntity;
+
+public class KirinGeoModel extends GeoModel<KirinEntity> {
+
+    @Override
+    public ResourceLocation getModelResource(KirinEntity animatable) {
+        return new ResourceLocation("srparasites", "geo/entity/kirin.geo.json");
+    }
+
+    @Override
+    public ResourceLocation getTextureResource(KirinEntity animatable) {
+        return new ResourceLocation("srparasites", "textures/entity/monster/kirin.png");
+    }
+
+    @Override
+    public ResourceLocation getAnimationResource(KirinEntity animatable) {
+        return new ResourceLocation("srparasites", "animations/entity/kirin.animation.json");
+    }
+
+    // For Class A-2 movement-driven animations, override codeAnimations:
+    // @Override
+    // public void codeAnimations(KirinEntity animatable, AnimatableManager<KirinEntity> manager) {
+    //     // Insert movement-driven animation code here
+    // }
+}
